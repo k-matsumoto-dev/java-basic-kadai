0994a23b7f5a15259af63d1d7fc93b3400e5df65
k-matsumoto-dev <kazuya-matsumoto@paramix.co.jp>
2025-05-26 12:53:59 +0900

    Java基礎 課題15再提出_1
    kadai_015パッケージを削除

--- kadai_015/CarExec_Chapter15.java
+++
@@ -1,9 +0,0 @@
-package kadai_015;
-
-public class CarExec_Chapter15 {
-    public static void main(String[] args) {
-        Car_Chapter015 car = new Car_Chapter015();
-        car.gearChange(3);
-        car.run();
-    }
-}
